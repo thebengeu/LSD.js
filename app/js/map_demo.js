ab8020ab309c6e1e782df667f0adf7828931db2a
shaohong <shaohong@shhcode.net>
2014-02-16 06:57:17 +0800

    test for text

--- app/js/map_demo.js
+++ app/js/map_demo.js
@@ -64,7 +64,8 @@
       for (var i in data['businesses']){
         var tmpLat = data['businesses'][i]['location']['coordinate']['latitude'];
         var tmpLong = data['businesses'][i]['location']['coordinate']['longitude'];
-        var tmpMarker = new L.marker([tmpLat, tmpLong], {icon: blueIcon});
+        var tmpMarker = new L.marker([tmpLat, tmpLong], {icon: blueIcon} +
+          '<br><p>Hello world!<br />This is a nice popup.</p>');
         tmpMarker.bindPopup(data['businesses'][i]['name']);
         marker.push(tmpMarker);
         map.addLayer(tmpMarker);
